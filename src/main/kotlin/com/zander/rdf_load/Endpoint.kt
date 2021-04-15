1d45775e765bc43f3127580eb5576038f7ae10bc
UghZan <yayminecrafters@gmail.com>
2021-04-15 17:36:28 +0300

    изменил код создания вершин/узлов

--- src/main/kotlin/com/zander/rdf_load/Endpoint.kt
+++ src/main/kotlin/com/zander/rdf_load/Endpoint.kt
@@ -4,6 +4,7 @@ import org.apache.jena.riot.RDFDataMgr
 import org.apache.tinkerpop.gremlin.driver.Client
 import org.apache.tinkerpop.gremlin.driver.Cluster
 import org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal
+import org.apache.tinkerpop.gremlin.structure.util.empty.EmptyGraph
 
 class Endpoint(private val port: Int, private val path: String) {
 
@@ -12,7 +13,7 @@ class Endpoint(private val port: Int, private val path: String) {
         val cluster = Cluster.open("conf/remote-objects.yaml")
         val client = cluster.connect<Client>().alias("g")
 
-        val trav = traversal().withRemote("conf/remote-graph.properties")
+        val g = EmptyGraph.instance().traversal().withRemote("conf/remote-graph.properties")
 
         if (path.isEmpty())
             throw Exception("Path is empty")
@@ -24,10 +25,15 @@ class Endpoint(private val port: Int, private val path: String) {
 
         //загружаем субъекты ака узлы
         rdf.listSubjects().forEach {
-            val V = trav.addV().`as`(it.uri)
+            g.addV().property("uri", it.uri).iterate()
             names.add(it.uri)
-            client.submit(V)
             //println("added new vertex ${it.uri}")
+        }
+
+        val results = g.V()
+
+        for (m in results) {
+            System.out.println(m)
         }
 
         println("All vertices added.")
@@ -36,47 +42,45 @@ class Endpoint(private val port: Int, private val path: String) {
         rdf.listStatements().forEach {
             if (it.`object`.isLiteral) // если объект отношения есть литерал
             {
-                val V = trav.V().`as`(it.subject.uri)
-                V.property(it.predicate.uri, it.`object`.asLiteral().value.toString()) // добавляем в граф как свойство
-                client.submit(V)
-                //println("added new property")
-            } else if (it.`object`.isURIResource) //если объект отношения есть другой объект (ресурс)
+                println("literal: ${it.`object`.asLiteral().value}")
+                g.V().has("uri", it.subject.uri).property(it.predicate.uri, it.`object`.asLiteral().value.toString()).iterate() // добавляем в граф как свойство
+            }
+            else if (it.predicate.localName == "type")
             {
-                if (names.contains(it.`object`.asResource().uri)) { //и имеется узел с таким URI
-                    val V1 = trav.V().`as`(it.subject.uri)
-                    val V2 = trav.V().`as`(it.`object`.asResource().uri)
-                    V1.addE(it.predicate.uri).to(V2) //создаем связь
+                g.V().has("uri", it.subject.uri).property(it.predicate.uri, it.`object`.asResource().uri).iterate() // добавляем в граф как свойство
+            }
+            else if (it.`object`.isURIResource) //если объект отношения есть другой объект (ресурс)
+            {
+                val uri = it.`object`.asResource().uri
 
-                    client.submit(V1)
-                    //println("added new edge")
-                } else //иначе
+                if (!names.contains(uri)) //если нет узла с таким URI
                 {
-                    val V = trav.V().`as`(it.subject.uri) //добавляем как свойство
-                    V.property(it.predicate.uri, it.`object`.asResource().uri)
-
-                    client.submit(V)
-                    //println("added new property")
+                    g.addV().property("uri", uri).iterate()
+                    names.add(uri)
                 }
+                val V1 = g.V().has("uri", it.subject.uri)
+                val V2 = g.V().has("uri", uri)
+                V1.addE(it.predicate.uri).to(V2).iterate() //создаем связь
             }
         }
 
         println("All edges and properties added.")
 
-        val results = trav.V()
+        //val results = g.V()
 
-        for (m in results) {
+        /*for (m in results) {
             System.out.println(m)
-        }
+        }*/
 
-        //trav.iterate()
+        //g.iterate()
 
         //println("Iteration completed.")
 
-        //client.submit(trav.)
+        //client.submit(g)
 
         println("Submitted.")
 
-        trav.close()
+        g.close()
         client.close()
         cluster.close()
     }
